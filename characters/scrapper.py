d614f29ae29d885cd712c7d3270e28732cf02e96
alexg <alexgubarev885@gmail.com>
2024-10-16 14:46:46 +0300

    Fixed save_characters to handle integrity error ( new character created only , if not exist with api_id in DB)

--- characters/scrapper.py
+++ characters/scrapper.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 import requests
+from django.db import IntegrityError
 
 from characters.models import Character
 
@@ -24,7 +25,10 @@ def scrape_characters() -> list[Character]:
 
 def save_characters(characters: list[Character]) -> None:
     for character in characters:
-        character.save()
+        try:
+            character.save()
+        except IntegrityError:
+            print("Character already exists")
 
 
 def sync_characters_with_api() -> None:
